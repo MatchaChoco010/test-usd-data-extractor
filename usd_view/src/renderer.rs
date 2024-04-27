69f5b6b3dbf05324daf5fc4acfab844af367791b
Orito Itsuki <20170107+MatchaChoco010@users.noreply.github.com>
2024-04-27 16:03:10 +0900

    Refactor egui window

--- usd_view/src/renderer.rs
+++ usd_view/src/renderer.rs
@@ -13,6 +13,7 @@ pub struct Renderer<'a> {
     egui_renderer: egui_wgpu::Renderer,
 
     usd_filename: String,
+    usd_time_code: i32,
 }
 
 impl<'a> Renderer<'a> {
@@ -87,6 +88,7 @@ impl<'a> Renderer<'a> {
             egui_state: egui_state,
             egui_renderer,
             usd_filename: String::new(),
+            usd_time_code: 1,
         }
     }
 
@@ -120,93 +122,114 @@ impl<'a> Renderer<'a> {
                 label: Some("Render Encoder"),
             });
 
-        {
-            let _render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
-                label: Some("Render Pass"),
-                color_attachments: &[Some(wgpu::RenderPassColorAttachment {
-                    view: &view,
-                    resolve_target: None,
-                    ops: wgpu::Operations {
-                        load: wgpu::LoadOp::Clear(wgpu::Color {
-                            r: 0.1,
-                            g: 0.2,
-                            b: 0.3,
-                            a: 1.0,
-                        }),
-                        store: wgpu::StoreOp::Store,
-                    },
-                })],
-                depth_stencil_attachment: None,
-                occlusion_query_set: None,
-                timestamp_writes: None,
-            });
-        }
+        {}
+
+        // main rendering
+        self.render_main_pass(&view, &mut encoder);
 
         // egui rendering
-        {
-            let raw_input = self.egui_state.take_egui_input(window);
-
-            let usd_filename = &mut self.usd_filename;
-
-            let full_output = self.egui_context.run(raw_input, |ui| {
-                egui::Window::new("Control Panel").show(ui, |ui| {
-                    ui.horizontal(|ui| {
-                        ui.text_edit_singleline(usd_filename);
-                        if ui.button("Click me!").clicked() {
-                            println!("Clicked!");
-                        }
-                    });
-                });
-            });
-
-            self.egui_state
-                .handle_platform_output(window, full_output.platform_output);
-
-            let tris = self
-                .egui_context
-                .tessellate(full_output.shapes, window.scale_factor() as f32);
-            for (id, image_delta) in &full_output.textures_delta.set {
-                self.egui_renderer
-                    .update_texture(&self.device, &self.queue, *id, &image_delta);
-            }
-
-            let screen_descriptor = egui_wgpu::ScreenDescriptor {
-                size_in_pixels: [self.size.width, self.size.height],
-                pixels_per_point: window.scale_factor() as f32,
-            };
-            self.egui_renderer.update_buffers(
-                &self.device,
-                &self.queue,
-                &mut encoder,
-                &tris,
-                &screen_descriptor,
-            );
-            let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
-                color_attachments: &[Some(wgpu::RenderPassColorAttachment {
-                    view: &view,
-                    resolve_target: None,
-                    ops: wgpu::Operations {
-                        load: wgpu::LoadOp::Load,
-                        store: wgpu::StoreOp::Store,
-                    },
-                })],
-                depth_stencil_attachment: None,
-                label: Some("egui main render pass"),
-                timestamp_writes: None,
-                occlusion_query_set: None,
-            });
-            self.egui_renderer
-                .render(&mut render_pass, &tris, &screen_descriptor);
-            drop(render_pass);
-            for x in &full_output.textures_delta.free {
-                self.egui_renderer.free_texture(x)
-            }
-        }
+        self.render_egui_pass(window, &view, &mut encoder);
 
         // submit will accept anything that implements IntoIter
         self.queue.submit(std::iter::once(encoder.finish()));
         output.present();
 
         Ok(())
+    }
+
+    fn render_main_pass(&mut self, view: &TextureView, encoder: &mut CommandEncoder) {
+        let _render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+            label: Some("Render Pass"),
+            color_attachments: &[Some(wgpu::RenderPassColorAttachment {
+                view: &view,
+                resolve_target: None,
+                ops: wgpu::Operations {
+                    load: wgpu::LoadOp::Clear(wgpu::Color {
+                        r: 0.1,
+                        g: 0.2,
+                        b: 0.3,
+                        a: 1.0,
+                    }),
+                    store: wgpu::StoreOp::Store,
+                },
+            })],
+            depth_stencil_attachment: None,
+            occlusion_query_set: None,
+            timestamp_writes: None,
+        });
+    }
+
+    fn render_egui_pass(
+        &mut self,
+        window: &Window,
+        view: &TextureView,
+        encoder: &mut CommandEncoder,
+    ) {
+        let raw_input = self.egui_state.take_egui_input(window);
+
+        let usd_filename = &mut self.usd_filename;
+        let usd_time_code = &mut self.usd_time_code;
+
+        let full_output = self.egui_context.run(raw_input, |ui| {
+            egui::Window::new("Control Panel").show(ui, |ui| {
+                ui.horizontal(|ui| {
+                    ui.label("USD File: ");
+                    ui.text_edit_singleline(usd_filename);
+                    if ui.button("Load .usd file").clicked() {
+                        println!("Clicked!");
+                    }
+                });
+                ui.scope(|ui| {
+                    ui.style_mut().spacing.slider_width = 400.0;
+                    ui.horizontal(|ui| {
+                        ui.label("Time Code: ");
+                        ui.add(egui::Slider::new(usd_time_code, 0..=109));
+                    });
+                });
+            });
+        });
+
+        self.egui_state
+            .handle_platform_output(window, full_output.platform_output);
+
+        let tris = self
+            .egui_context
+            .tessellate(full_output.shapes, window.scale_factor() as f32);
+        for (id, image_delta) in &full_output.textures_delta.set {
+            self.egui_renderer
+                .update_texture(&self.device, &self.queue, *id, &image_delta);
+        }
+
+        let screen_descriptor = egui_wgpu::ScreenDescriptor {
+            size_in_pixels: [self.size.width, self.size.height],
+            pixels_per_point: window.scale_factor() as f32,
+        };
+        self.egui_renderer.update_buffers(
+            &self.device,
+            &self.queue,
+            encoder,
+            &tris,
+            &screen_descriptor,
+        );
+        let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+            color_attachments: &[Some(wgpu::RenderPassColorAttachment {
+                view: view,
+                resolve_target: None,
+                ops: wgpu::Operations {
+                    load: wgpu::LoadOp::Load,
+                    store: wgpu::StoreOp::Store,
+                },
+            })],
+            depth_stencil_attachment: None,
+            label: Some("egui main render pass"),
+            timestamp_writes: None,
+            occlusion_query_set: None,
+        });
+        self.egui_renderer
+            .render(&mut render_pass, &tris, &screen_descriptor);
+        drop(render_pass);
+        for x in &full_output.textures_delta.free {
+            self.egui_renderer.free_texture(x)
+        }
     }
 }
